a27bcb46fbf8eab94364001ab10c42a96880c559
sagar sen <sagarsen088@gmail.com>
2020-07-24 17:37:35 +0530

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Logger;  
+import org.apache.log4j.xml.DOMConfigurator;  
+  
+public class HelloWorld {  
+  
+    static Logger logger = Logger.getLogger(HelloWorld.class);  
+      
+    public static void main(String[] args) {  
+        DOMConfigurator.configure("log4j.xml");  
+        logger.debug("Sample debug message");  
+        logger.info("Sample info message");  
+        logger.warn("Sample warn message");  
+        logger.error("Sample error message");  
+        logger.fatal("Sample fatal message");  
+    }  
+}  
